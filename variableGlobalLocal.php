af86dc8f63caaf45531a4d943ae02a45b9fcfdf6
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-23 20:37:01 -0300

    What is global scope and local scope

---
+++ variableGlobalLocal.php
@@ -0,0 +1,20 @@
+<?php
+
+    //Variable global
+    // $valor = 34;
+    // function mostrar(){
+    //     echo "Valor de la variable".$valor;
+    // }
+
+    // mostrar();
+    // echo 'Valor de la variable'. $valor;
+  
+    //Variable local
+    function mostrar(){
+        $valor = 34;
+        echo "Valor de la variable local: ".$valor;
+    }  
+    mostrar();
+    echo "<p><strong>Mostrará error en linea 19 porque no esta declarada en el scope global:</strong></p>";
+    echo 'Valor de la variable'. $valor;
+?>
