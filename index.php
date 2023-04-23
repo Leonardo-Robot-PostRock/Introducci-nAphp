af52e6ee90b28ed560c8e930da1dd434b3064073
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-23 19:32:43 -0300

    concatenación

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP</title>
 </head>
+
 <body>
     <p>Mensaje desde php</p>
     <?php
@@ -17,5 +19,23 @@
     /* comentario desde php
      */
     ?>
+    <hr>
+
+    <p>
+        <strong>
+            Variables en php
+        </strong>
+    </p>
+    <?php
+    $texto = "Texto desde php";
+    $numero = 12;
+    $numero2 = 13;
+
+    echo "Mensaje: $texto <br>";
+    //Concatenación
+    echo "Mensaje:" . $texto . " " . $numero . " " . $numero2;
+
+    ?>
 </body>
+
 </html>
