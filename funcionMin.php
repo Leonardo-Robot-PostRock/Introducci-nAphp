67772ca67ccefd767f702df480ac1a58998546af
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-24 02:11:58 -0300

    funcion min

---
+++ funcionMin.php
@@ -0,0 +1,7 @@
+<?php
+
+//Función min()
+
+echo "<p>Recibe un listado de número y se encarga de decir cual es el menor de ellos</p>";
+echo "min(numeros)<br> Por ejemplo: min(12,23,3,67,98,9)<br>";
+echo "Salida: " . min(12,23,3,67,98,9);
