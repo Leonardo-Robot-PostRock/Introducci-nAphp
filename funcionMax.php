101a8791d51d9cc8b9d2216aaa5a1342134ef453
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-24 02:11:39 -0300

    funcion max

---
+++ funcionMax.php
@@ -0,0 +1,5 @@
+<?php
+//Funcion max()
+echo "<p>Recibe un listado de número y se encarga de decir cual es el mayor de ellos</p>";
+echo "max(numeros)<br> Por ejemplo: max(12,23,3,67,98,9)<br>";
+echo "Salida: " . max(12,23,3,67,98,9);
