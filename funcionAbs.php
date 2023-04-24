1765eb4a24df4f873bfee5f6d4e9eb1561832542
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-24 02:11:19 -0300

    funcion abs

---
+++ funcionAbs.php
@@ -0,0 +1,9 @@
+<?php
+
+//Función abs()
+
+$n = -123.3;
+
+echo "Convierte un número negativo a positivo, por ejemplo: -3.5 y n = -123.3 <br>";
+echo "Número positivo: ", abs(-3.5);
+echo "<br>Número positivo: ", abs($n);
