e2ed3c1f7a54dca75d14e0ed125fc24787d256a8
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-24 02:45:58 -0300

    funcion rand

---
+++ funcionRand.php
@@ -0,0 +1,8 @@
+<?php
+
+    echo "La función rand() elige un número aleatorio: <br>";
+    echo rand();
+
+    echo "<p>¡Se puede elegir los límites en rand()! Es decir desde y hasta que número</p>";
+    echo "<br>Por ejemplo: rand(num1, num2). Digamos que desde 1 hasta 10 <br>";
+    echo rand(1,10);
