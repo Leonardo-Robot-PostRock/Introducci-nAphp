c5babc5e2f0a49fcba541bdd1767b5e459edb4c2
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-24 02:46:14 -0300

    funcion round

---
+++ funcionRound.php
@@ -0,0 +1,5 @@
+<?php
+    echo "La función round() se encarga de redondear los números decimales";
+    echo "<br>Por ejemplo: 1.2 y 1.6 <br>Salida: ";
+    echo "<br>" . round(1.2) . "<br>" . round(1.6);
+?>
