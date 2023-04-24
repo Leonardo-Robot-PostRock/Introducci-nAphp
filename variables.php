8b917ca8172f2db3cf127fceae6512442b469905
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-24 02:45:36 -0300

    variables constantes

--- variables.php
+++ variables.php
@@ -7,5 +7,14 @@ $texto = "Texto desde php";
 $numero = 12;
 $numero2 = 13;
 
+//Cómo concatenar en PHP? -------------------------------------------------
 include('./concatenacion.php');
+
+//Variables constantes ----------------------------------------------------------
+echo "<h3>También se pueden definir variables de tipo constante</h3>";
+define("x", 56);
+echo "<br> La sintaxis es: define(nombreVariable, valor) <br> Ejemplo: define('x', 56) <br> Resultado: ";
+echo x;
+
+echo "<p><small>Debes tener en cuenta que si intentas cambiar el valor de la variable constante tendras un error</small></p>";
 ?>
