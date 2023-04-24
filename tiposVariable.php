276a6f29dde7a097d7cb08e33da6d13087d0e9ee
Leonardo Puebla <leonardo38361@gmail.com>
2023-04-23 21:22:52 -0300

    tipos de variables y arreglos

---
+++ tiposVariable.php
@@ -0,0 +1,40 @@
+<?php
+// TIPOS DE VARIABLES 
+
+// CADENA -------------------------------------------
+
+$cadena = "mensaje";
+var_dump($cadena);
+
+echo "<br>";
+
+// NUMERO ENTERO INT -------------------------------------------------
+
+$numero = 12;
+var_dump($numero);
+
+echo "<br>";
+
+// NUMERO DECIMAL FLOAT -------------------------------------------------
+
+$numeroDecimal = 12.44343;
+var_dump($numeroDecimal);
+
+echo "<br>";
+
+// BOOLEANOS -------------------------------------------------
+
+$encendidoBoolean = true;
+var_dump($encendidoBoolean);
+
+echo "<br>";
+
+// ARREGLOS ---------------------------------------------------
+
+$dias = array("lunes", "martes", "miércoles");
+
+var_dump($dias);
+
+//Funciones para trabajar con cadenas --------------------------------
+
+include('./funcionesParaTrabajarCadenas.php');
